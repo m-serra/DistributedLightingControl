c9642d7f49e4935c787b53b2c95b200539c01a05
m-serra <manuelserranunes@gmail.com>
2018-12-03 22:38:50 +0000

    communicatig via a text file

--- tcp_comm/example.cpp
+++ tcp_comm/example.cpp
@@ -13,7 +13,10 @@
 #include <memory>
 #include <utility>
 #include <boost/asio.hpp>
+#include <fstream>
 
+
+using namespace std;
 using boost::asio::ip::tcp;
 
 class session
@@ -38,15 +41,55 @@ class session
                 [this, self](boost::system::error_code ec, std::size_t length){
                 
                 if (!ec){
-                    printf("Received: %s\n", data_);
+                    printf("Request: %s\n", data_);
                     do_write(length);
                 }
             });
         }
 
         void do_write(std::size_t length){
+			
+			ifstream myfile;
+			myfile.open ("i1.txt");
+			
+            if(!myfile.is_open()) {
+				cerr << "Error: " << strerror(errno);
+				return;
+			}
+            std::string line;
+            std::getline(myfile, line);
+            myfile.close();
+            
+            std::cout << "Reply: " << line << "\n\n";
+            /*if(fin.is_open()) {
+				fin.seekg(-1,ios_base::end); // go to one spot before the EOF
+
+				bool keepLooping = true;
+				while(keepLooping) {
+					char ch;
+					fin.get(ch);            // Get current byte's data
+
+					if((int)fin.tellg() <= 1) { // If the data was at or before the 0th byte
+						fin.seekg(0);           // The first line is the last line
+						keepLooping = false;    // So stop there
+					}
+					else if(ch == '\n') {       // If the data was a newline
+						keepLooping = false;    // Stop at the current position.
+					}
+					else {                      // If the data was neither a newline nor at the 0 byte
+						fin.seekg(-2,ios_base::cur);   // Move to the front of that data, then to the front of the data before it
+					}
+				}
+			
+				string lastLine;            
+				getline(fin,lastLine);                      // Read the current line
+				cout << "Result: " << lastLine << '\n';     // Display it
+
+				fin.close();
+			}*/
+			
             auto self(shared_from_this());
-            boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
+            boost::asio::async_write(socket_, boost::asio::buffer(line.c_str(), 8),
                 [this, self](boost::system::error_code ec, std::size_t /*length*/){
                     
                     if (!ec){
@@ -56,7 +99,7 @@ class session
         }
 
     tcp::socket socket_;
-    enum { max_length = 1024 };
+    enum { max_length = 8 };
     char data_[max_length];
 };
 
