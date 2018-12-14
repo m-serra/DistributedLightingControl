c0f7d213e1cdc24d4f33f76133a8b7da60698c38
Unknown <filipaoliveirarente@gmail.com>
2018-12-14 16:10:51 +0000

    switch

--- switch_button/switch_button.ino
+++ switch_button/switch_button.ino
@@ -1,9 +1,7 @@
 volatile boolean occupancy_state = false;
-volatile boolean occupancy_state_prev = false;
-const byte interruptPin = 2;
 volatile int buttonState = 0;
 const int debounceDelay = 2;
-volatile float previous_t = 0.0, curr_t = 0.0;
+volatile float prev_t = 0.0, curr_t = 0.0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,19 +13,22 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:  
-  if(occupancy_state != occupancy_state_prev){
-    if(occupancy_state){Serial.println("OCCUPIED!");}
-    else{Serial.println("Empty");}
-  }
-
-  occupancy_state_prev = occupancy_state;
 }
 
 void button_pressed(){
 
   curr_t = micros();
   buttonState = digitalRead(interruptPin);
-  if(buttonState == 1 && ((curr_t - prev_t)> 10000)){occupancy_state = !occupancy_state;}
+  if(buttonState == 1 && ((curr_t - prev_t)> 10000)){
+    
+    //Change in occupancy state!
+    occupancy_state = !occupancy_state;
+    
+    //Print
+    if(occupancy_state){Serial.println("Changed state from FREE to OCCUPIED!");}
+    else{Serial.println("Changed state from OCCUPIED to FREE!");}
+    
+    }
 
   prev_t = micros();
    
